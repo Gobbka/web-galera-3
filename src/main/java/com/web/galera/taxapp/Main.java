0292ce30513320cbc91a725b479bf217a0fc98aa
Nikitin A <nikitin_a@codekig.ru>
2025-10-08 17:13:50 +0300

    clean up code

--- src/main/java/com/web/galera/taxapp/Main.java
+++ src/main/java/com/web/galera/taxapp/Main.java
@@ -1,12 +1,10 @@
 package com.web.galera.taxapp;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void main(String[] args) {
         var scanner = new Scanner(System.in);
     }
 
